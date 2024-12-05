670b4d4af6a67380132c853809bd9ad6f676e89f
DigitalDetective47 <ninji2701@gmail.com>
2024-12-05 18:53:24 -0500

    finish malverk mod

---
+++ localization/default.lua
@@ -0,0 +1,21 @@
+return {
+    descriptions = {
+        alt_texture = {
+            alt_tex_sus_sus = {
+                c_sun = {
+                    name = "The Sus"
+                }
+            }
+        },
+        texture_packs = {
+            texpack_sus_sus = {
+                name = "When the impostor is Sun",
+                text = {
+                    "Renames {C:tarot}The Sun{} tarot",
+                    "card to {C:tarot}The Sus",
+                    "and replaces its texture",
+                }
+            }
+        }
+    }
+}
---
+++ main.lua
@@ -0,0 +1,17 @@
+AltTexture({
+    key = 'sus',
+    set = 'Tarot',
+    path = 'sus.png',
+    keys = {
+        'c_sun'
+    },
+    localization = {
+        'c_sun'
+    }
+})
+TexturePack({
+    key = 'sus',
+    textures = {
+        'sus_sus'
+    }
+})
